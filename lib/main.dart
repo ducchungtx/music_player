4c66a3f286db23e836ea0469e0269a1bee17743b
Chung Nguyen <zrmedia9@gmail.com>
2023-12-19 21:17:53 +0700

    Project init

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_player/common/color_extension.dart';
+import 'package:music_player/view/splash_view.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Music Player",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Circular Std",
+        scaffoldBackgroundColor: TColor.bg,
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: TColor.primaryText,
+          displayColor: TColor.primaryText,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: TColor.primary,
+        ),
+        useMaterial3: false,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
